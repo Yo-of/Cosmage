a02391c4267025ca3e7b6c700bbf7060703b6802
Yo <mohammedlouali27@gmail.com>
2026-04-18 23:16:48 +0100

    Updated things

---
+++ script.js
@@ -0,0 +1,5 @@
+const PLANETS = {
+    earth: { name: "Earth", yearDays: 365.25, dayHours: 24, symbol: "🌍", fact: "Earth completes one orbit around the Sun every 365.25 days. Our entiry calendar is built around this very single cosmic cycle." },
+    mercury: { name: "Mercury", yearDays: 87.97, dayHours: 1407.6, symbol: "☿", fact: "A Mercury day (from sunrise to sunset) lasts around 176 Earth days which is longer than its year! Temperatures there swings between -180°C and 430°C." },
+    venus: { name: "Venus", yearDays: 224.7, dayHours: 5832, symbol:"♀", fact: "Venus rotates backwards (we can also say retrograde), so the Sun rise from the west." }
+}
